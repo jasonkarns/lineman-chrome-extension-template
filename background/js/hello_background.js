5b9929ab8cbbfdd4ccd37c3542caaa0883304540
Jason Karns <jason@karns.name>
2013-10-14 10:37:20 -0400

    Remove browserAction from stub stuff since Chrome extension APIs are dumb

--- background/js/hello_background.js
+++ background/js/hello_background.js
@@ -1,6 +1,4 @@
 // sample background script
 // CoffeeScript is also supported in this directory
 
-chrome.browserAction.onClicked.addListener(function(tab){
-  console.log("Hello, World!");
-});
+console.log("Hello, World!");
